5af30349f9aa9d21b0ef4c2292ab7eef028dfdc6
Ruwaid Imran <ruwaidmran@gmail.com>
2023-11-29 20:26:29 +0500

    router practice

---
+++ react-router-mine/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+
+
+    </div>
+  );
+}
+
+export default App;
