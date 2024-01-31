ab5db00d519d74826382b17bb7c3aaaef6e7cb0b
Ruwaid Imran <ruwaidmran@gmail.com>
2024-01-31 17:25:51 +0500

    add menu

--- navbar/src/component/Navbar.jsx
+++ navbar/src/component/Navbar.jsx
@@ -1,19 +1,29 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
 const Navbar = () => {
+  const[menuOpen, SetmenuOpen]= useState(false)
 return (
+
 <nav className="Nav-bar">
-  <ul>
-    <li><Link to="/">Website</Link></li>
-    <li><Link to="/Services">Services</Link></li>
-    <li><Link to="/Contact">Contact</Link></li>
-    <li><Link to="/About">About</Link></li>
+
+<li><Link to="/">Website</Link></li>
+<div className="menu">
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+  <ul className={menuOpen ?  "open": ""}>
+    <li><NavLink to="/Services">Services</NavLink></li>
+    <li><NavLink to="/Contact">Contact</NavLink></li>
+    <li><NavLink to="/About">About</NavLink></li>
   </ul>
 
 </nav>
+
 )
 }
 
--- navbar/src/index.js
+++ navbar/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 //eslint-disable-next-line
 import { BrowserRouter,withRouter } from 'react-router-dom';
+import'../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
